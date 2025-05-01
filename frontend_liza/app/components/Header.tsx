876572e133a135e1d62c016a302bc424e64521dc
Liza Tinku Jose <123905707+lizatinku@users.noreply.github.com>
2025-05-01 16:02:42 -0700

    added a nice logo

--- frontend_liza/app/components/Header.tsx
+++ frontend_liza/app/components/Header.tsx
@@ -1,15 +1,27 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="w-full px-6 py-4 flex items-center justify-between bg-[#fdf9f1]">
-      <div className="text-sm font-semibold text-black">LOGO</div>
+      {/* LOGO IMAGE */}
+      <div className="flex items-center">
+        <Image
+          src="/assets/SignBridgeLogo.png"
+          alt="SignBridge logo"
+          width={90}
+          height={90}
+          className="mr-2"
+        />
+      </div>
 
+      {/* TITLE */}
       <h1 className="text-3xl font-semibold text-center text-black flex-1 -ml-8">
-        ASL TRANSLATOR
+        SIGN BRIDGE
       </h1>
 
+      {/* LINK */}
       <Link
         href="/how-it-works"
         className="text-sm text-black font-semibold hover:underline transition"
@@ -17,6 +29,5 @@ export default function Header() {
         How it Works
       </Link>
     </header>
-
   );
 }
