5ee0e02899238e4733cb51c634221b543e4bbdb4
Ameen Salim <ameensalim1@gmail.com>
2025-05-15 13:52:33 -0700

    added instructions for running letters model locally

--- American-Sign-Language-Detection/flaskapp.py
+++ American-Sign-Language-Detection/flaskapp.py
@@ -109,4 +109,4 @@ def handle_frame(data):
             })
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
